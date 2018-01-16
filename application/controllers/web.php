f36edc4f530a5600abb0783cbf92573794169677
ferina umu <ferinaumusalamah@student.uns.ac.id>
2018-01-16 14:26:34 +0700

    versi 1.0

---
+++ application/controllers/web.php
@@ -0,0 +1,21 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+  /**
+   *
+   */
+  class Web extends CI_Controller
+  {
+
+    function __construct()
+    {
+      parent::__construct();
+
+  		// $this->load->model('LoginM');
+    }
+
+    function index()
+    {
+      $this->load->view('web/index');
+    }
+  }
+
+?>
---
+++ application/views/web/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <link rel="stylesheet" id="udefault-css" href="http://localhost/stp/assets/css/default.css" type="text/css" media="all"> -->
+    <link rel="stylesheet" id="styles-css" href="http://localhost/stp/assets/css/styles_002.css" type="text/css" media="all">
+    <link rel="stylesheet" id="style-css" href="http://localhost/stp/assets/css/style_3.css" type="text/css" media="all">
+    <!-- <link rel="stylesheet" id="youtube-channel-gallery-css" href="http://localhost/stp/assets/css/styles.css" type="text/css" media="all"> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/stp/assets/css/main.css" />
+    <link rel="stylesheet" href="http://localhost/stp/assets/css/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+
+    <style>
+    /* Note: Try to remove the following lines to see the effect of CSS positioning */
+    .affix {
+        top: 0;
+        width: 100%;
+        z-index: 9999 !important;
+    }
+
+    .affix + .container-fluid {
+        padding-top: 70px;
+    }
+    </style>
+    <script>
+  	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  	ga('create', 'UA-71541642-1', 'auto');
+  	ga('require', 'displayfeatures');
+  	ga('send', 'pageview');
+  	</script>
+
+    <title></title>
+  </head>
+  <body>
+    <div class="container-fluid page" style="background-image:url('http://localhost/stp/assets/img/indonesia2.jpg')">
+      <div class="grid-10 tablet-grid-10 mobile-grid-30 b-r logo-holder">
+        <a href="http://localhost/stp/"><img src="http://localhost/stp/assets/img/logostp.png" alt=""></a>
+      </div>
+      <div class="grid-50 tablet-grid-60 mobile-grid-70">
+        <div class="brand">
+          <h3 class="site-title" style="font-size:15px">WHERE <br>COMPETENCE <br>INOVATIVE <br>TECHNOLOGY <br>AND BUSINESS GROW</h3>
+        </div><!-- brand -->
+      </div>
+      <div class="grid-20 tablet-grid-20 hide-on-mobile">
+        <form role="search" method="get" id="searchform" action="https://ristekdikti.go.id/" class="search-top" style="margin:0;">
+          <input name="s" id="s" type="text">
+          <input value="" type="submit">
+        </form>
+      </div>
+    </div>
+
+    <div class="main-nav" style="background-color:#4286f4" data-spy="affix" data-offset-top="197">
+      <div class="grid-container" style="background-color:#4286f4">
+        <nav style="background-color:#4286f4">
+          <div class="menu text-uppercase">
+            <ul class=" horizontal no-padder">
+              <li class=" current-menu-item"><a href="#">Home</a></li>
+              <li><a href="#">Profil</a>
+                <ul class="sub-menu">
+                  <li><a href="#">yuuu</a></li>
+                  <li><a href="#">wkwkwk</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div style="height:1000px">
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+      <h1>Some text to enable scrolling</h1>
+    </div>
+
+    <div class="footer">
+      <div class="footer-content">
+        <p>Contact Us :</p>
+        <br> <p>wkwkwkwk</p>
+        <br> <p>uuuu</p>
+        <br><br><br> <p>popopopop</p>
+      </div>
+    </div>
+  </body>
+</html>
