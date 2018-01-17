53c70d150da81012d38061c23ee02506247f93e2
rahmatiah <rahmatiahnurrifaarif@gmail.com>
2018-01-17 09:17:08 +0700

    versi 1.3

--- application/views/web/index.php
+++ application/views/web/index.php
@@ -2,14 +2,21 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="http://localhost/stp/assets/css/slider/icon.css" rel="stylesheet">
+    <link href="http://localhost/stp/assets/css/slider/bootstrap.min.css" rel="stylesheet">
+	  <link rel="stylesheet" href="http://localhost/stp/assets/css/slider/font-awesome.min.css">
+	  <link href="http://localhost/stp/assets/css/slider/animate.min.css" rel="stylesheet">
+    <link href="http://localhost/stp/assets/css/slider/prettyPhoto.css" rel="stylesheet">
+	  <link href="http://localhost/stp/assets/css/slider/main.css" rel="stylesheet">
+    <link href="http://localhost/stp/assets/css/slider/responsive.css" rel="stylesheet">
     <link rel="stylesheet" id="styles-css" href="http://localhost/stp/assets/css/styles_002.css" type="text/css" media="all">
     <link rel="stylesheet" id="style-css" href="http://localhost/stp/assets/css/style_3.css" type="text/css" media="all">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://localhost/stp/assets/css/main.css" />
     <link rel="stylesheet" href="http://localhost/stp/assets/css/style.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
     <style>
     /* Note: Try to remove the following lines to see the effect of CSS positioning */
@@ -60,44 +67,97 @@
           <div class="menu text-uppercase">
             <ul class=" horizontal no-padder">
               <li class=" current-menu-item"><a href="#">Home</a></li>
-              <li><a href="#">Profil</a></li>
-              <li><a href="#">Program</a></li>
-              <li><a href="#">Berita</a></li>
-              <li><a href="#">Solo Science Center</a></li>
-              <li><a href="#">Diklat Mekanik</a>
+              <li><a href="#">Profil</a>
                 <ul class="sub-menu">
-                  <li><a href="#">Berita</a></li>
-                  <li><a href="#">Pendaftaran</a></li>
+                  <li><a href="#">yuuu</a></li>
+                  <li><a href="#">wkwkwk</a></li>
                 </ul>
               </li>
-              <li><a href="#">Inkubator Bisnis</a>
-                <ul class="sub-menu">
-                  <li><a href="#">Berita</a></li>
-                  <li><a href="#">Pendaftaran</a></li>
-                  <li><a href="#">Profil Tenant dan List Produk</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Kontak</a></li>
+              <li><a href="#">Page 2</a></li>
+              <li><a href="#">Page 3</a></li>
             </ul>
           </div>
         </nav>
       </div>
     </div>
 
-    <div style="height:1000px">
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-      <h1>Some text to enable scrolling</h1>
-    </div>
+    <div class="container" style="width:1250px">
+    <div class="slider" style="margin-top:30px">
+        <div class="col-sm-6">
+        <div id="about-slider">
+          <div id="carousel-slider" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+              <ol class="carousel-indicators visible-xs">
+                <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-slider" data-slide-to="1"></li>
+                <li data-target="#carousel-slider" data-slide-to="2"></li>
+              </ol>
 
+            <div class="carousel-inner" style="height:300px;max-width:100%">
+              <div class="item active">
+                <img src="http://localhost/stp/assets/img/slider_one.jpg" class="img-responsive" alt="">
+               </div>
+               <div class="item">
+                <img src="http://localhost/stp/assets/img/slider_one.jpg" class="img-responsive" alt="">
+               </div>
+               <div class="item">
+                <img src="http://localhost/stp/assets/img/slider_one.jpg" class="img-responsive" alt="">
+               </div>
+            </div>
+
+            <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
+              <i class="fa fa-angle-left"></i>
+            </a>
+
+            <a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next">
+              <i class="fa fa-angle-right"></i>
+            </a>
+          </div> <!--/#carousel-slider-->
+        </div><!--/#about-slider-->
+      </div><!--/.col-md-4-->
+    </div>
+    <div class="col-sm-6">
+      <body-menu>
+        <input id="tab1" type="radio" name="tabs" checked>
+        <label for="tab1">PENGUMUMAN</label>
+
+        <input id="tab2" type="radio" name="tabs">
+        <label for="tab2">SIARAN PERS</label>
+
+        <input id="tab3" type="radio" name="tabs">
+        <label for="tab3">BERITA</label>
+
+        <section-menu id="content1">
+          <p-1>
+            Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+          </p-1>
+          <p-1>
+            Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+          </p-1>
+        </section-menu>
+
+        <section-menu id="content2">
+          <p-1>
+            Bacon ipsum dolor sit amet landjaeger sausage brisket, jerky drumstick fatback boudin ball tip turducken. Pork belly meatball t-bone bresaola tail filet mignon kevin turkey ribeye shank flank doner cow kielbasa shankle. Pig swine chicken hamburger, tenderloin turkey rump ball tip sirloin frankfurter meatloaf boudin brisket ham hock. Hamburger venison brisket tri-tip andouille pork belly ball tip short ribs biltong meatball chuck. Pork chop ribeye tail short ribs, beef hamburger meatball kielbasa rump corned beef porchetta landjaeger flank. Doner rump frankfurter meatball meatloaf, cow kevin pork pork loin venison fatback spare ribs salami beef ribs.
+          </p-1>
+          <p-1>
+            Jerky jowl pork chop tongue, kielbasa shank venison. Capicola shank pig ribeye leberkas filet mignon brisket beef kevin tenderloin porchetta. Capicola fatback venison shank kielbasa, drumstick ribeye landjaeger beef kevin tail meatball pastrami prosciutto pancetta. Tail kevin spare ribs ground round ham ham hock brisket shoulder. Corned beef tri-tip leberkas flank sausage ham hock filet mignon beef ribs pancetta turkey.
+          </p-1>
+        </section-menu>
+
+        <section-menu id="content3">
+          <p-1>
+            Bacon ipsum dolor sit amet beef venison beef ribs kielbasa. Sausage pig leberkas, t-bone sirloin shoulder bresaola. Frankfurter rump porchetta ham. Pork belly prosciutto brisket meatloaf short ribs.
+          </p-1>
+          <p-1>
+            Brisket meatball turkey short loin boudin leberkas meatloaf chuck andouille pork loin pastrami spare ribs pancetta rump. Frankfurter corned beef beef tenderloin short loin meatloaf swine ground round venison.
+          </p-1>
+        </section-menu>
+      </boyd-menu>
+    </div>
+  </div><!--/.container-->
+
+<section>
     <div class="footer">
       <div class="footer-content">
         <p>Contact Us :</p>
@@ -106,5 +166,14 @@
         <br><br><br> <p>popopopop</p>
       </div>
     </div>
+  </section>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://localhost/stp/assets/js/jquery.js"></script>
+    <script src="http://localhost/stp/assets/js/bootstrap.min.js"></script>
+    <script src="http://localhost/stp/assets/js/jquery.prettyPhoto.js"></script>
+    <script src="http://localhost/stp/assets/js/jquery.isotope.min.js"></script>
+    <script src="http://localhost/stp/assets/js/wow.min.js"></script>
+  <script src="http://localhost/stp/assets/js/main.js"></script>
   </body>
 </html>
